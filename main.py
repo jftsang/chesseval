4a8eba1e230690def7ebdd754713153300594fca
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2025-11-17 17:36:29 +0000

    updates, use new pgn viewer for frontend

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import asyncio
-import uuid
-from io import StringIO
-from typing import Any, Coroutine
+from pathlib import Path
 
 import chess
 import chess.engine
 import chess.pgn
 import fastapi
-import pydantic
 import uvicorn
 from fastapi import HTTPException
 from starlette.requests import Request
-from starlette.responses import Response, JSONResponse
+from starlette.responses import Response
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-from streamerate import stream
 
 ENGINE = "/Users/jmft2/.local/bin/stockfish.exe"
 default_fen = chess.STARTING_FEN
@@ -40,23 +36,17 @@ app.mount(
 templates = Jinja2Templates(directory="templates")
 
 
-def sanitize_povscore(ps: chess.engine.PovScore) -> str:
-    white_score = ps.white()
-    mate = white_score.mate()
+def sanitize_povscore(score: chess.engine.Score) -> str:
+    mate = score.mate()
     if mate is not None:
-        if mate > 0:
-            return f"+M{mate}"
-        elif mate < 0:
-            return f"-M{mate}"
-        else:
-            raise ValueError
+        return f"M{mate}"
     else:
-        return f"{white_score.score()/100:+.2f}"
+        return f"{score.score()/100:+.2f}"
 
 
 def sanitize_infodict(board: chess.Board, infod: chess.engine.InfoDict) -> dict:
     san = infod.copy()
-    san["score"] = sanitize_povscore(san["score"])
+    san["score"] = sanitize_povscore(san["score"].white())
 
     san["continuation"] = []
     board = board.copy()
@@ -69,92 +59,95 @@ def sanitize_infodict(board: chess.Board, infod: chess.engine.InfoDict) -> dict:
     return san
 
 
-class GameInfo(pydantic.BaseModel):
-    scores: list[str]
-    moves: list[chess.Move]
+class GameManager:
+    directory: Path = Path(__file__).parent / "games"
 
+    def load(self, key: str) -> chess.pgn.Game:
+        p = self.directory / key
+        if not p.exists():
+            raise HTTPException(status_code=404, detail="Game not found")
+        if not p.parent == self.directory:
+            raise HTTPException(status_code=403, detail="Game not found")
 
-class GameReview:
-    def __init__(self, game: chess.pgn.Game) -> None:
-        self.game: chess.pgn.Game = game
-        self.ready: bool = False
+        with open(p) as f:
+            game = chess.pgn.read_game(f)
+        return game
 
-    async def do_review(self) -> None:
-        if self.ready:
-            return
+    def save(self, key: str, game: chess.pgn.Game) -> None:
+        p = self.directory / key
+        if not p.parent == self.directory:
+            raise HTTPException(status_code=403, detail="Game not found")
 
+        with open(p, "w") as f:
+            f.write(self.as_pgn(game))
+
+    def as_pgn(self, game):
+        exporter = chess.pgn.StringExporter()
+        output = game.accept(exporter)
+        return output
+
+    async def create_review(self, key: str) -> chess.pgn.Game:
+        game = self.load(key)
         transport, engine = await chess.engine.popen_uci(ENGINE)
 
-        async def _eval_inner(cn: chess.pgn.GameNode) -> None:
-            board = cn.board()
+        async def _eval_inner(
+            node: chess.pgn.GameNode, prevscore: chess.engine.PovScore | None
+        ) -> None:
+            # Evaluate the board at this node, which is the state AFTER
+            # a move.
+
+            board = node.board()
             info = await engine.analyse(board, chess.engine.Limit(time=0.1))
-            score = sanitize_povscore(info["score"])
-            cn.comment = score
+            score = info["score"]
+            node.comment = sanitize_povscore(score.white())
+
+            if prevscore is not None:
+                # board.turn is the turn of the player about to take the
+                # next move, so we need its inverse if we want to evaluate
+                # the move that has just happened
+
+                # checkmate is worth 10000 pawns
+                prev_num = prevscore.pov(not board.turn).score(mate_score=10000)
+                cur_num = score.pov(not board.turn).score(mate_score=10000)
+
+                # a bad move will have a positive deficit
+                deficit = prev_num - cur_num
+
+                if deficit >= 500:  # loses a major piece
+                    node.nags.add(chess.pgn.NAG_BLUNDER)
+                elif deficit >= 300:  # loses a minor piece
+                    node.nags.add(chess.pgn.NAG_MISTAKE)
+                elif deficit >= 100:  # loses a pawn
+                    node.nags.add(chess.pgn.NAG_DUBIOUS_MOVE)
 
             coros = []
-            for var in cn.variations:
-                coros.append(_eval_inner(var))
+            for var in node.variations:
+                coros.append(_eval_inner(var, score))
             await asyncio.gather(*coros)
 
         try:
-            await _eval_inner(self.game)
-            self.ready = True
+            await _eval_inner(game, prevscore=None)
+            return game
         finally:
             await engine.quit()
 
-    def as_pgn(self):
-        exporter = chess.pgn.StringExporter()
-        output = self.game.accept(exporter)
-        return output
 
-
-reviews: dict[str, GameReview] = {}
-
-
-@app.post("/review")
-async def create_review(pgn: str) -> JSONResponse:
-    h = uuid.uuid4().hex
-    game: chess.pgn.Game = chess.pgn.read_game(StringIO(pgn))
-    reviews[h] = GameReview(game)
-    asyncio.create_task(reviews[h].do_review())
-    return JSONResponse(h, status_code=201)
-
-
-@app.get("/reviews")
-async def list_reviews() -> list[dict]:
-    # return stream(reviews.items()).starmap(lambda key, gr: (key, gr.ready)).to_list()
-    return [{"h": h, "ready": gr.ready} for h, gr in reviews.items()]
-
-
-@app.get("/reviews/{gid}")
-async def get_review(gid: str) -> str | None:
-    try:
-        game_review = reviews[gid]
-    except KeyError:
-        raise HTTPException(status_code=404, detail="Review not found")
-    if not game_review.ready:
-        return None
-
-    game = game_review.game
-    exporter = chess.pgn.StringExporter()
-    output = game.accept(exporter)
-    return output
-
-
-@app.get("/view")
-async def view(request: Request) -> Response:
-    pgn = open("games/yahoo.pgn").read()
-    return templates.TemplateResponse("board.html", {"request": request, "pgn": pgn})
-
-
-@app.get("/view/{h}")
-async def view(request: Request, h: str) -> Response:
-    try:
-        gr = reviews[h]
-    except KeyError:
-        raise HTTPException(status_code=404, detail="Review not found")
+@app.get("/review/{key}")
+async def get_review(request: Request, key: str) -> Response:
+    gm = GameManager()
+    game = gm.load(key)
+    reviewed_game = await gm.create_review(key)
     return templates.TemplateResponse(
-        "board.html", {"request": request, "pgn": gr.as_pgn()}
+        "board.html", {"request": request, "pgn": gm.as_pgn(reviewed_game)}
+    )
+
+
+@app.get("/view/{key}")
+async def view(request: Request, key: str) -> Response:
+    gm = GameManager()
+    game = gm.load(key)
+    return templates.TemplateResponse(
+        "board.html", {"request": request, "pgn": gm.as_pgn(game)}
     )
 
 
